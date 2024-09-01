e59303a2e908829ad2b47c605e662ce020a9d6ae
Saba Salukvadze <saluqvadze2006@gmail.com>
2024-09-01 18:40:59 +0400

    Add project files.

---
+++ Qvortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Qvortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
