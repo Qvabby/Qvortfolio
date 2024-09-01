ea568d0d73db955c0d17ff425a720c59a710d22f
Saba Salukvadze <saluqvadze2006@gmail.com>
2024-09-01 19:17:31 +0400

    Index Page Animation start.

--- Qvortfolio/wwwroot/js/site.js
+++ Qvortfolio/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    
+    $("#q").slideUp(0).delay(200).fadeIn(250).animate({
+        top: '40px'
+    });
+    $("#v").slideUp(0).delay(450).fadeIn(300).animate({
+        top: '80px'
+    });
+    $("#a").slideUp(0).delay(750).fadeIn(350).animate({
+        top: '120px'
+    });
+    $("#b").slideUp(0).delay(1100).fadeIn(400).animate({
+        top: '165px'
+    });
+    $("#b2").slideUp(0).delay(1500).fadeIn(450).animate({
+        top: '209px'
+    });
+    $("#y").slideUp(0).delay(1950).fadeIn(500).animate({
+        top: '242px'
+    });
+    $("#t").slideUp(0).delay(2450).fadeIn(550).animate({
+        top: '296px'
+    });
+    $("#e").slideUp(0).delay(3000).fadeIn(600).animate({
+        top: '330px'
+    });
+});
