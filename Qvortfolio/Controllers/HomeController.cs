c1edf2c5ac3183e5cb3b0c5183cf40fe7ab0f2d9
Saba Salukvadze <saluqvadze2006@gmail.com>
2025-02-27 02:49:52 +0400

    Redesigning with Sesili's recommendations.

--- Qvortfolio/Controllers/HomeController.cs
+++ Qvortfolio/Controllers/HomeController.cs
@@ -20,6 +20,45 @@ namespace Qvortfolio.Controllers
 
         public async Task<IActionResult> Index()
         {
+   //         var configuration = new ConfigurationBuilder()
+   //.SetBasePath(Directory.GetCurrentDirectory())
+   //.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+   //.Build();
+   //         var client = new GitHubClient(new ProductHeaderValue("Qvortfolio"));
+   //         var basicAuth = new Credentials(configuration["GitHub:Username"], configuration["Github:Password"]);
+   //         client.Credentials = basicAuth;
+   //         //getting user repositories.
+   //         var repositories = await client.Repository.GetAllForUser("Qvabby");
+   //         //turning them into viewmodels
+   //         var repositoryModels = repositories.Select(r => new GithubRepositoryViewModel
+   //         {
+   //             Name = r.Name,
+   //             Description = r.Description,
+   //             HtmlUrl = r.HtmlUrl
+   //         }).ToList();
+            //return View(repositoryModels);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult CV()
+        {
+            return View();
+        }
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+        public IActionResult PublishedWorks()
+        {
+            return View();
+        }
+        public async Task<IActionResult> GithubRepositories()
+        {
             var configuration = new ConfigurationBuilder()
    .SetBasePath(Directory.GetCurrentDirectory())
    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
@@ -37,16 +76,6 @@ namespace Qvortfolio.Controllers
                 HtmlUrl = r.HtmlUrl
             }).ToList();
             return View(repositoryModels);
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-        public IActionResult CV()
-        {
-            return View();
         }
     }
 }
